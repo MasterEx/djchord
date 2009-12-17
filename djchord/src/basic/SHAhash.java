d187fbe0ece13a6b23b86c9c15100d9ec75097ce
Periklis Ntanasis <periklis@debian-netbook.(none)>
2009-12-18 01:04:16 +0200

    SHAhash and SHA1 implementation

--- djchord/src/basic/SHAhash.java
+++ djchord/src/basic/SHAhash.java
@@ -33,6 +33,95 @@ package basic;
  *
  * @author Ntanasis Periklis and Chatzipetros Mike
  */
-public class SHAhash {
+public class SHAhash implements Comparable{
+
+    private byte[] bytehash = new byte[40];
+    private String stringhash;
+
+    /*
+     * constructor
+     */
+    SHAhash(byte[] hash)
+    {
+        this.bytehash = hash;
+        this.stringhash = convertToHex(hash);
+    }
+
+    /*
+     * compareTo implementation
+     */
+    public int compareTo(Object arg0)
+    {
+        SHAhash hash = (SHAhash) arg0;
+        for(int i=0;i<40;i++)
+        {
+            if (bytehash[i]>hash.getByte(i))
+            {
+                return 1;
+            }
+            else if (bytehash[i]<hash.getByte(i))
+            {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    /*
+     * returns bytehash
+     */
+    public byte[] getByteHash()
+    {
+        return bytehash;
+    }
+
+    /*
+     * returns specific byte ofbytehash
+     */
+    public byte getByte(int position)
+    {
+        return bytehash[position];
+    }
+
+    /*
+     * returns stringhash
+     */
+    public String getStringHash()
+    {
+        return stringhash;
+    }
+
+    private String convertToHex(byte[] data)
+    {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < data.length; i++)
+        {
+            int halfbyte = (data[i] >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do
+            {
+                if ((0 <= halfbyte) && (halfbyte <= 9))
+                {
+                    buf.append((char) ('0' + halfbyte));
+                }
+                else
+                {
+                    buf.append((char) ('a' + (halfbyte - 10)));
+                }
+                halfbyte = data[i] & 0x0F;
+            }
+            while(two_halfs++ < 1);
+        }
+        return buf.toString();
+    }
+
+    /*
+     * overides toString()
+     */
+    @Override
+    public String toString()
+    {
+        return stringhash;
+    }
 
 }
