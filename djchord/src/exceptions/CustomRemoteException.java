49fafaccc1c0f3978b89623fedca37634389ac9c
mike <mikeoeo@gmail.com>
2010-01-23 16:10:09 +0200

    i have doubts about the custom exception...

---
+++ djchord/src/exceptions/CustomRemoteException.java
@@ -0,0 +1,64 @@
+/**
+ * Ntanasis Periklis - A.M.:3070130
+ * Chatzipetros Mike - A.M.:3070175
+ *
+ * check LICENSE.txt in the parent directory
+ *
+ * The MIT License
+ *
+ * Copyright (c) 2009 Ntanasis Periklis and Chatzipetors Mike
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package exceptions;
+
+import chord.RemoteNode;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Ntanasis Periklis and Chatzipetros Mike
+ */
+public class CustomRemoteException extends RemoteException {
+    private int i=0,o=0;
+    private String s;
+    private RemoteNode successor,predecessor;
+    /**
+     * 
+     * o=1 successor, o=2 predecessor
+     */
+    public CustomRemoteException()
+    {
+        super();
+    }
+    
+    public void handle(int o,RemoteNode node)
+    {
+        if(o==1)
+        {
+            this.successor = node;
+        }
+        else if(o==2)
+        {
+            this.predecessor = node;
+        }
+    }
+
+}
